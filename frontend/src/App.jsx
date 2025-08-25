2a37a9adeac0668a06d47e82d2dd93674e24fc65
Sharon <sharonsharon9048461583@gmail.com>
2025-08-25 11:26:34 +0530

    fixed the bug in backend server.js and app.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,40 +7,17 @@ import Navbar from './Components/Navbar/Navbar';
 import Dashboard from './Pages/Dashboard/Dashboard.jsx';
 import ProtectedRoute from './routes/ProtectedRoutes.jsx';
 import Signup from "./Pages/Signup/Signup.jsx" 
-import { useEffect } from 'react';
-import { setUser } from './redux/slices/authslice.js';
-import { useDispatch } from 'react-redux';
 import Upload from './Components/uploadImages/upload.jsx';
 import Edit from './Components/EditProfile/Edit.jsx';
 import AdminDashBoard from './Pages/AdminDashBoard/AdminDashBoard.jsx';
 import AdminLogin from './Components/AdminLogin/AdminLogin.jsx';
 import EditUser from './Components/EditAccessadmin/EditAdmin.jsx';
 import Serchuser from './Components/serch/Serch-user.jsx';
-import AdminProtectedRoute from './routes/AdminProtectedRoutes.jsx';
 import AdminCreateUser from './Pages/createUser/AdminCreateUser.jsx';
 
 function App() {
 
-//  const dispatch = useDispatch();
-//  useEffect (() => {
 
-//   const fetchProfile = async () => {
-//   try {
-//     const response = await fetch("http://localhost:5006/api/users/profile" , {
-//       method : "GET" ,
-//       credentials : "include",
-//     })
-//     const data = await response.json();
-    
-//     if ( response.ok ) {
-//       dispatch(setUser(data.user));
-//     }
-//   }catch ( error ) {
-//     console.error("error fetching profile error!");
-//   }
-//   }
-//   fetchProfile(); 
-//  },[dispatch])
 
   return (
     <BrowserRouter>
